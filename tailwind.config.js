5c145064a5af6db482f9c49830be25e8dfdcc1e8
Attila Max Ruf <therufa@gmail.com>
2021-04-20 23:20:03 +0200

    chore: install fastify

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.{vue,js,ts,tsx}',
+    './public/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
