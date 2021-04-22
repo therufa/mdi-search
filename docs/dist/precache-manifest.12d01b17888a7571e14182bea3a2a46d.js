342d43c1553cd38a66859e26b3fac154a0da32d5
Attila Max Ruf <therufa@gmail.com>
2021-04-23 01:37:29 +0200

    chore: upgrade public path

---
+++ docs/dist/precache-manifest.12d01b17888a7571e14182bea3a2a46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51680859af66ca7a90a2",
+    "url": "/mdi-search/css/app.f53a7c8d.css"
+  },
+  {
+    "revision": "2c3b5cf45ff46cb56cd3",
+    "url": "/mdi-search/css/chunk-vendors.f6add958.css"
+  },
+  {
+    "revision": "456f9904fb6247ea260794add41a63b2",
+    "url": "/mdi-search/index.html"
+  },
+  {
+    "revision": "7edde7ffbd7e01937f9f",
+    "url": "/mdi-search/js/about.c1db5f18.js"
+  },
+  {
+    "revision": "51680859af66ca7a90a2",
+    "url": "/mdi-search/js/app.6d55750c.js"
+  },
+  {
+    "revision": "2c3b5cf45ff46cb56cd3",
+    "url": "/mdi-search/js/chunk-vendors.34c48130.js"
+  },
+  {
+    "revision": "57ac3a9f3eed324ac24d806ba9f6d508",
+    "url": "/mdi-search/manifest.json"
+  }
+]);
