2230b174c3fffbef7872725c988af413f0bed75e
Attila Max Ruf <therufa@gmail.com>
2021-04-23 00:52:56 +0200

    fix: config ports

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  apps : [{
-    name: "api",
-    script: "./api/server.js",
-    instances: "max",
+  apps: [{
+    name: 'api',
+    script: './api/server.js',
+    instances: 'max',
     env: {
-      NODE_ENV: "development",
+      NODE_ENV: 'development',
       PORT: 3000
     },
     env_production: {
-      NODE_ENV: "production",
-      PORT: 80
+      NODE_ENV: 'production',
     }
   }]
 }
