776f466f9bc8ae12d741a2d2bb001bcb348d337c
Attila Max Ruf <therufa@gmail.com>
2021-04-23 01:31:45 +0200

    chore: build dist

---
+++ dist/precache-manifest.442e16256854a69ae3a268476459499e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134f138c2f238e3c59b4",
+    "url": "/css/app.f53a7c8d.css"
+  },
+  {
+    "revision": "2c3b5cf45ff46cb56cd3",
+    "url": "/css/chunk-vendors.f6add958.css"
+  },
+  {
+    "revision": "4febdec3970da2baa31742c98961bc28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7edde7ffbd7e01937f9f",
+    "url": "/js/about.c1db5f18.js"
+  },
+  {
+    "revision": "134f138c2f238e3c59b4",
+    "url": "/js/app.d324aaaa.js"
+  },
+  {
+    "revision": "2c3b5cf45ff46cb56cd3",
+    "url": "/js/chunk-vendors.34c48130.js"
+  },
+  {
+    "revision": "57ac3a9f3eed324ac24d806ba9f6d508",
+    "url": "/manifest.json"
+  }
+]);
